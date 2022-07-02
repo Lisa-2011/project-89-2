b4ce8b8e75f4c8e8cc64b34fb29a5787a56be87d
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2022-07-01 20:58:55 -0400

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,6 +1,6 @@
 function addUser(){
-    player_1=document.getElementById("player_1_user").value;
-    player_2=document.getElementById("player_2_user").value;
+    player_1_user=document.getElementById("player_1_user").value;
+    player_2_user=document.getElementById("player_2_user").value;
 
     localStorage.setItem("player_1_user",player_1_user);
     localStorage.setItem("player_2_user",player_2_user);
